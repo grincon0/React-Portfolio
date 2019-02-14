845c0b471ded716f8f94c7b16d693865ec9eec7c
Wake <georgerincon2@gmail.com>
2019-02-14 16:59:53 -0500

    resized canvas to reflect desired product + poly testing

--- client/src/helpers/PtsHelper.js
+++ client/src/helpers/PtsHelper.js
@@ -1,11 +1,12 @@
-import { CanvasSpace, Num, Pt, Group } from "pts";
+import { CanvasSpace, Num,Rectangle,  Pt, Group } from "pts";
 
 
-export default class SpecialEffect{
+export default class SpecialEffect {
+    //Space provides the paper, Form provides the pencil, and Point provides the idea
 
     initializeCanvas = () => {
         const space = new CanvasSpace("#canvas");
-        space.setup({ bgcolor: "#e2097d"});
+        space.setup({ bgcolor: "#e2097d" });
         this.initializeForm(space);
 
     }
@@ -21,10 +22,19 @@ export default class SpecialEffect{
 
     }
     createPointAnimation = (space, form) => {
-        space.add( (time, ftime) => {
-            let radius = Num.cycle( (time%1000)/1000 ) * 20;
-            form.fill("#09f").point( space.pointer, radius, "circle" );
-          });
+        space.add((time, ftime) => {
+            let radius = Num.cycle((time % 1000) / 1000) * 20;
+            form.fill("#09f").point(space.pointer, radius, "circle");
+        });
+        //this.createTestPoly(space, form);
+    }
+    createTestPoly = (space, form, time) => {
+        let rect = Rectangle.fromCenter(space.center, space.size.$divide(2));
+        let poly = Rectangle.corners(rect);
+        poly.shear2D(Num.cycle(time % 5000 / 5000) - 0.5, space.center);
+
+        form.fillOnly("#123").polygon(poly);
+        form.strokeOnly("#fff", 3).rect(rect);
     }
 
 }
