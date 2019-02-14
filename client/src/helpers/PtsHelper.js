728fae7bc8777793071a45467940a6eccba240ab
Wake <georgerincon2@gmail.com>
2019-02-14 04:12:15 -0500

    Pts canvas pointer-tester sucessful

--- client/src/helpers/PtsHelper.js
+++ client/src/helpers/PtsHelper.js
@@ -3,19 +3,25 @@ import { CanvasSpace, Pt, Group } from "pts";
 
 export default class SpecialEffect{
 
+
     initializeCanvas = () => {
         const space = new CanvasSpace("#canvas");
-        space.setup({ bgcolor: "#fff"});
+        space.setup({ bgcolor: "#e2097d"});
         this.initializeForm(space);
 
     }
     initializeForm = (space) => {
-        const form = space.getForm();
-        this.createTestPoint(space, form);
+        let form = space.getForm();
+        //space._hasMouse = true;
+        space.add( () => form.point( space.pointer, 10 ) );
+        space.play().bindMouse().bindTouch();
+        console.log(space);
+        //this.createTestPoint(space, form);
 
     }
     createTestPoint = (space, form) => {
-        space.add( () => form.point( space.pointer, 10));
+        space.add( () => form.point( space.pointer, 10 ) );
 
     }
+
 }
