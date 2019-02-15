420081b4be30b073ca43d246347a16eef2f93ccf
Wake <georgerincon2@gmail.com>
2019-02-15 02:28:18 -0500

    multiple colored vectors working

--- client/src/helpers/PtsHelper.js
+++ client/src/helpers/PtsHelper.js
@@ -1,8 +1,14 @@
-import { CanvasSpace, Num, Rectangle, Line, Tempo, Const,  Pt, Group } from "pts";
+import { CanvasSpace, Num, Rectangle, Line, Tempo, Const, Pt, Group } from "pts";
 
 
 export default class SpecialEffect {
     //Space provides the paper, Form provides the pencil, and Point provides the idea
+    constructor() {
+        this.points = [];
+        this.lines = [];
+        this.colors = ["#fe0000","#fdfe02", "#0bff01", "#011efe", "#fe00f6"];
+
+    }
 
     initializeCanvas = (type) => {
         const space = new CanvasSpace("#canvas");
@@ -19,15 +25,15 @@ export default class SpecialEffect {
 
         space.play().bindMouse().bindTouch();
 
-        if(type === "poly"){
+        if (type === "poly") {
             this.createPolyAnimation(space, form);
-        }else if(type === "line"){
-            this.createVectors(space, form);
+        } else if (type === "line") {
+            this.createPoints(space, form);
         }
-        else{
+        else {
             this.createPointAnimation(space, form);
         }
-        
+
     }
     createPointAnimation = (space, form) => {
         space.add((time, ftime) => {
@@ -41,43 +47,49 @@ export default class SpecialEffect {
             let rect = Rectangle.fromCenter(space.center, space.size.$divide(2));
             let poly = Rectangle.corners(rect);
             poly.shear2D(Num.cycle(time % 5000 / 5000) - 0.5, space.center);
-    
+
             form.fillOnly("#123").polygon(poly);
             form.strokeOnly("#fff", 3).rect(rect);
         });
 
     }
+    createPoints = (space, form) => {
+        while (this.points.length <= 15) {
+            let temp = {};
+            temp["x"] = this.getRandomX();
+            temp["y"] = this.getRandomY();
+            temp["color"] = this.colors[Math.floor(Math.random() * 5)];
+            this.points.push(temp);
+
+        }
+        this.createVectors(space, form)
+    }
+    getRandomY = () => {
+        return Math.floor(Math.random() * 700) + 100;
+    }
+    getRandomX = () => {
+        return Math.floor(Math.random() * 3000) + 1000;
+    }
     createVectors = (space, form) => {
         space.add((time, ftime) => {
-/*             let rect = Rectangle.fromTopLeft([50,50], 100, 50);
-            form.fill("#003082").rect(rect);*/
-            let cycle = (off) => space.center.x* ( Num.cycle( (time+off)%4000/5000 ) * 2);
-            let endpoint = space.center - 1;
-            let poin = new Pt([cycle(1000) ,300]);
-            let poin2 = new Pt([1100, 300]);
-            let ln = new Line(poin, poin2);
-            let kedo = Line.fromAngle( poin, 0, 40);
+            let cycle = (off) => space.center.x * (Num.cycle((time + off) % 3000 / 5000) * 2);
 
-            form.strokeOnly( 0, 10, "round", "round" ).line(kedo);
+            for(let i = 0; i < this.points.length; i++){
+                let tempPoint = new Pt(cycle(this.points[i].x), this.points[i].y);
+                form
+                .strokeOnly(this.points[i].color, 10, "round", "round")
+                .line(Line.fromAngle(tempPoint, 0, 40));
+            }
 
 
+/*             let poin = new Pt([cycle(1000), 300]);
+            let poin2 = new Pt([cycle(2000), 500]);
+            let kedo = Line.fromAngle(poin, 0, 40);
+            let kedo2 = Line.fromAngle(poin2, 0, 40); */
 
-            //let ln = Line.fromAngle( space.center.$add( cycle(1000), 0 ), 75, space.size.y/3  );
-            //let ln = Line.fromAngle( space.center ,75, space.size.y/3 );
+/*             form.strokeOnly(0, 10, "round", "round").line(kedo);
+            form.strokeOnly("#f00", 10, "round", "round").line(kedo2); */
 
-            //let ln = Line.fromAngle( space.center.$add( cycle(1000), 0 ), 75, space.size.y/3  );
-
-            //let radius = Num.cycle( (time%3000)/3000 ) * 20;
-/*             form
-            .fill("#09f")
-            .point(  space.center.$subtract( cycle(1000), 0 ), 20, "circle" ); */
-
-            
-            
-
-            //form.strokeOnly("#ed0b0b", 10, "round", "round").line( ln );
-            //console.log(ln);
-            
         });
     }
 
