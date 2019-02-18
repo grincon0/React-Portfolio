172a700e191ba5d6cdb9a05d0dab8070e71fbf24
Wake <georgerincon2@gmail.com>
2019-02-17 23:19:49 -0500

    created working button-esque div at landing...1st version

---
+++ client/src/components/Clickable/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Clickable = (props) => {
+    return (
+        <div className="clickable">
+            <p style={{ fontSize: "20px", display:"inline", left:"35%"}}>See my work</p>
+                <i className="fa fa-arrow-right turn-arrow " aria-hidden="false"></i>
+        </div>
+    );
+}
+
+export default Clickable;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Container from "../components/Container/index";
 import Canvas from "../components/Canvas/index";
+import Clickable from "../components/Clickable/index";
 import Col from "../components/Col/index";
 import Row from "../components/Row/index";
 import PtsHelper from "../helpers/PtsHelper";
 import Slider from "../components/Slider/index";
 import Footer from "../components/Footer/index";
+
 import "./styles/Landing.css";
 
 class Landing extends Component {
@@ -34,13 +36,18 @@ class Landing extends Component {
                         <Col size="md-6">
                             <Canvas>
                                 <div className="center">
-                                    <p>Hello, I'm <span className="name">George Rincon</span>.</p>
-                                    <p>I'm a full-stack developer.</p>
+                                    <p className="center-reg-text">Hello, I'm <span className="name">George Rincon</span>.</p>
+                                    <p className="center-reg-text">I'm a full-stack developer.</p>
+                                    <Clickable />
                                 </div>
+                                
                             </Canvas>
                         </Col>
                     </Row>
+                    <Row>
+                    </Row>
                 </Container>
+
                 <Footer />
             </div>
 
