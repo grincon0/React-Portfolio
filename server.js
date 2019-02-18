c8f7702cd40729e9fe37d5fc879667985ac12b12
Wake <georgerincon2@gmail.com>
2019-02-18 01:18:38 -0500

    rebased due to fatal dependecy error

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+//const routes = require('./routes');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+
+// Add routes, both API and View
+//app.use(routes);
+
+// Start the API server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
