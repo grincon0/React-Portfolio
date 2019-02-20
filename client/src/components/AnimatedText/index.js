e711b884fa519f1cc978f679d5c294dbabd9c642
Wake <georgerincon2@gmail.com>
2019-02-20 02:08:13 -0500

    text animation successful

--- client/src/components/AnimatedText/index.js
+++ client/src/components/AnimatedText/index.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 
 
-const AnimatedText = (props) => {
-
-    let animation;
-
-    if(props.anim === "slide-right"){
-        animation = props.anim;
+class AnimatedText extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            animation: '',
+            animationPlay: false
+        }
     }
 
-    return(
-        <div>
-            <p className={"default-text" + animation}>{props.text}</p>
-        </div>
-    );
+    componentDidMount = () => {
+        this.timerID = setTimeout(() => {
+            this.handleGetAnimation();
+        }, 4000);
+
+    }
+    componentWillUnmount = () => {
+        clearTimeout(this.timerID);
+    }
+    handleGetAnimation = () => {
+        if(this.props.anim === "slide-right"){
+                this.setState({
+                    animation: this.props.anim,
+                    animationPlay: true
+                });
+        }else{
+            console.log('no');
+        }
+    }
+    render = () => {
+        return (
+            <div>
+                <p className={"default-text " + (this.state.animation ? this.state.animation : "")}>{this.props.text}</p>
+            </div>
+        );
+    }
 
 }
 
