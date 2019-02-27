7d053716f0cd831ce6b73918ccaf9c50be10a6f5
George <georgerincon2@gmail.com>
2019-02-27 03:10:55 -0500

    Custom collapsable now slides it and out on btn click

--- client/src/components/CustomNavbar/index.js
+++ client/src/components/CustomNavbar/index.js
@@ -4,27 +4,95 @@ import "./style.css";
 
 class CustomNavBar extends Component {
     state = {
-        collapsed: false
+        class: "noshow",
+        listClass:"nowshow",
+        hasCollapseBtnClicked: false,
+        canCollapse: false
     }
-    componentDidMount = () => {
+    componentDidUpdate = () => {
+        console.log(this.state);
+    }
+    handleCollapse = () => {
+        //changes c0llapse state
+        if (!this.state.hasCollapseBtnClicked) {
+            let newState = {
+ 
+                hasCollapseBtnClicked: true
+            }
+            this.handleNewState(newState);
+        } else {
+            this.handleNewState();
+        }
+    }
+    handleNewState = (newState) => {
+        console.log("state changing");
+        if (newState !== undefined) {
 
+            if (!this.state.canCollapse) {
+                newState.class = "collapsee";
+                newState.canCollapse = true;
+                this.setState(newState);
+            }
+
+        } else {
+            if (!this.state.canCollapse) {
+                this.setState({ class:"collapsee", canCollapse: true });
+            } else {
+                this.setState({ class:"hidee", canCollapse: false });
+            }
+        }
+
+    }
+
+    cleanOutClass = (classToRemove) => {
+        document.getElementById("CustomCollapse").classList.remove(classToRemove);
+    }
+    elementHasClass = (classToCheck) => {
+        document.getElementById("CustomCollapse").classList.contains(classToCheck);
+    }
+    handleCollapsableDisplay = () => {
+        document.getElementsById("CustomCollapse").style.display = "block";
     }
     render = () => {
         return (
-            <nav className="CustomNav">
-                <div className="CustomAnchors">
-                    <a href="#">HOME</a>
-                    <a href="#">ABOUT</a>
-                    <a href="#">PORTFOLIO</a>
-                    <a href="#">CONTACT</a>
+            <nav>
+                <div className="CustomNav">
+                    <div className="CustomAnchors">
+                        <a href="#">HOME</a>
+                        <a href="#">ABOUT</a>
+                        <a href="#">PORTFOLIO</a>
+                        <a href="#">CONTACT</a>
+                    </div>
+
+                    <div>
+                        <button className="CustomNavBtn" onClick={this.handleCollapse}>
+                            <img src={MenuIcon} width={40} height={41} alt="icon"></img>
+                        </button>
+                    </div>
                 </div>
                 <div>
-                <button className="CustomNavBtn">
-                    <img src={MenuIcon} width={40} height={41} alt="icon"></img>
-                </button>
-                </div>
+                    <div id="CustomCollapse" className={`NavMenu ${this.state.class} `}>
 
+                        <ul className={this.state.listClass}> 
+                            <li>
+                                <a href="#">HOME</a>
+                            </li>
+                            <li>
+                                <a href="#">ABOUT</a>
+                            </li>
+                            <li>
+                                <a href="#">PORTFOLIO</a>
+                            </li>
+                            <li>
+                                <a href="#">CONTACT</a>
+                            </li>
+                        </ul>
+
+
+                    </div>
+                </div>
             </nav>
+
         );
     }
 }
