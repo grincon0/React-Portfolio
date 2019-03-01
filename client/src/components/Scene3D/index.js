6dde49c95d345cb5b940a1251f65f08d89dec0ff
George <georgerincon2@gmail.com>
2019-03-01 03:57:31 -0500

    working on 3d img opacity edits

--- client/src/components/Scene3D/index.js
+++ client/src/components/Scene3D/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./style.css";
 
+const textStyle = {
+    transform: [{ rotate: '0deg' }]
+}
+
 
 class Scene3D extends Component {
     constructor(props) {
@@ -14,16 +18,23 @@ class Scene3D extends Component {
     }
     render = () => {
         return (
-            <div className="scene">
-                <div className="parent-container">
-                    <div className={`container-3d ${this.state.addclass ? "container-3d-origin" : ""}`}>
-                        <div className="card-3d" onClick={this.handleStateChange}>
-                            <img className="image-marker" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
-                            <img className="image-marker" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
-                             <img className="image-marker" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
-                            <img className="image-marker" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" /> 
+            <div>
+                <div className="sub-text">
+                    <p id="text1"className="subtitle">Home</p>
+                    <p id="text2" className="subtitle">About</p>
+                    <p id="text3" className="subtitle">Labs</p>
+                    <p  id="text4" className="subtitle">Contact</p>
+                </div>
+                <div className="scene">
+                    <div className="parent-container">
+                        <div className={`container-3d ${this.state.addclass ? "container-3d-origin" : ""}`}>
+                            <div className="card-3d" onClick={this.handleStateChange}>
+                                <img id="image1" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                                <img id="image2" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                                <img id="image3" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                                <img id="image4" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
