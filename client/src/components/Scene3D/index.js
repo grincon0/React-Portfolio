372cc6ae9c084967ac71e50086224eef736a832d
George <georgerincon2@gmail.com>
2019-03-02 22:56:34 -0500

    3d card expansion sucessful...will test and edit more later

--- client/src/components/Scene3D/index.js
+++ client/src/components/Scene3D/index.js
@@ -1,38 +1,79 @@
 import React, { Component } from "react";
 import "./style.css";
 
-const textStyle = {
-    transform: [{ rotate: '0deg' }]
-}
-
-
 class Scene3D extends Component {
     constructor(props) {
         super(props);
         this.state = {
             addclass: false,
+            releaseContain: false,
+            imageClicked: '',
         }
     }
-    handleStateChange = () => {
-        this.state.addclass ? this.setState({ addclass: false }) : this.setState({ addclass: true });
+    handleStateChange = (e) => {
+        let ident = e.target.id;
+/*         let newState ={...this.state};
+        this.state.addclass ? newState.addclass = false : newState.addclass = true; */
+        this.state.addclass ? this.setState({ addclass: false, releaseContain:false }) : this.setState({ addclass: true, releaseContain:true });
+        this.handleSelectionOpacity(ident);
+        this.handleImageExpansion(ident);
+    }
+    handleImageExpansion = (id) => {
+        console.log(id);
+        document.getElementById(id).classList.remove("image-size");
+
+        document.getElementById(id).classList.add("expand");
+        document.getElementById(id).classList.add("start-anim");
+        document.getElementById("card-container").classList.remove("container-3d-width");
+        document.getElementById("card-container").classList.remove("overflow-hide-all");
+
+    }
+    handleSelectionOpacity = (id) => {
+        switch (id) {
+            case "image1":
+            document.getElementById("image2").classList.add("disappear");
+            document.getElementById("image3").classList.add("disappear");
+            document.getElementById("image4").classList.add("disappear");
+
+                break;
+            case "image2":
+            document.getElementById("image1").classList.add("disappear");
+            document.getElementById("image3").classList.add("disappear");
+            document.getElementById("image4").classList.add("disappear");
+                break;
+            case "image3":
+            document.getElementById("image1").classList.add("disappear");
+            document.getElementById("image2").classList.add("disappear");
+            document.getElementById("image4").classList.add("disappear");
+                break;
+            case "image4":
+            document.getElementById("image1").classList.add("disappear");
+            document.getElementById("image2").classList.add("disappear");
+            document.getElementById("image3").classList.add("disappear");
+                break;
+            default:
+                break;
+        }
+
     }
     render = () => {
         return (
             <div>
                 <div className="sub-text">
-                    <p id="text1"className="subtitle">Home</p>
+                    <p id="text1" className="subtitle">Home</p>
                     <p id="text2" className="subtitle">About</p>
                     <p id="text3" className="subtitle">Labs</p>
-                    <p  id="text4" className="subtitle">Contact</p>
+                    <p id="text4" className="subtitle">Contact</p>
                 </div>
                 <div className="scene">
                     <div className="parent-container">
-                        <div className={`container-3d ${this.state.addclass ? "container-3d-origin" : ""}`}>
-                            <div className="card-3d" onClick={this.handleStateChange}>
-                                <img id="image1" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
-                                <img id="image2" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
-                                <img id="image3" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
-                                <img id="image4" src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                        <div id="card-container" className={`container-3d ${this.state.releaseContain ? "" : "container-3d-width"} overflow-hide-all ${this.state.addclass ? "container-3d-origin" : ""}`}>
+                            <div className="card-3d" >
+                                <img data-ident="1" id="image1" className="image-size image-opacity" onClick={this.handleStateChange} src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                                <img data-ident="2" id="image2" className="image-size image-opacity" onClick={this.handleStateChange} src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                                <img data-ident="3" id="image3" className="image-size image-opacity" onClick={this.handleStateChange} src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" alt="atl" />
+                                <img data-ident="4" id="image4" className="image-size image-opacity image-position-4"   onClick={this.handleStateChange} src="https://codyhouse.co/demo/3d-animated-mockup/img/cd-app-image.png" 
+                                alt="atl" />
                             </div>
                         </div>
                     </div>
