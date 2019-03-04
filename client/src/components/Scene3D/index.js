f2041e439e18523e6a91327db0562239461e4926
George <georgerincon2@gmail.com>
2019-03-04 17:26:00 -0500

    all card have slide in anims that fill screen for desktop

--- client/src/components/Scene3D/index.js
+++ client/src/components/Scene3D/index.js
@@ -16,121 +16,168 @@ class Scene3D extends Component {
     }
     handleStateChange = (e) => {
         let ident = e.target.id;
-/*         let newState ={...this.state};
-        this.state.addclass ? newState.addclass = false : newState.addclass = true; */
-        this.state.addclass ? this.setState({ addclass: false, releaseContain:false }) : this.setState({ addclass: true, releaseContain:true });
+        /*         let newState ={...this.state};
+                this.state.addclass ? newState.addclass = false : newState.addclass = true; */
+        this.state.addclass ? this.setState({ addclass: false, releaseContain: false }) : this.setState({ addclass: true, releaseContain: true });
         this.handleSelectionOpacity(ident);
         this.handleImageExpansion(ident);
     }
     handleImageExpansion = (id) => {
         console.log(id);
-        if(this.state.releaseContain){
+        if (this.state.releaseContain) {
             document.getElementById(id).classList.add("image-size");
             document.getElementById(id).classList.remove("expand");
-            document.getElementById(id).classList.remove("start-anim");
+
+            this.handleImageAnimation(id, "remove");
+            /* document.getElementById(id).classList.remove("start-anim"); */
             document.getElementById("card-container").classList.add("container-3d-width");
             document.getElementById("card-container").classList.add("overflow-hide-all");
-        }else{
+        } else {
             document.getElementById(id).classList.remove("image-size");
             document.getElementById(id).classList.add("expand");
-            document.getElementById(id).classList.add("start-anim");
+            this.handleImageAnimation(id, "add");
+            /* document.getElementById(id).classList.add("start-anim"); */
             document.getElementById("card-container").classList.remove("container-3d-width");
             document.getElementById("card-container").classList.remove("overflow-hide-all");
         }
 
 
     }
-    handleSelectionOpacity = (id) => {
+    handleImageAnimation = (id, action) => {
 
-        if(this.state.releaseContain){
+        if (action === "remove") {
             switch (id) {
                 case "image1":
-                document.getElementById("image2").classList.add("disappear");
-                document.getElementById("image3").classList.add("disappear");
-                document.getElementById("image4").classList.add("disappear");
-    
+                    document.getElementById(id).classList.remove("start-animD");
                     break;
                 case "image2":
-                document.getElementById("image1").classList.add("disappear");
-                document.getElementById("image3").classList.add("disappear");
-                document.getElementById("image4").classList.add("disappear");
+                    document.getElementById(id).classList.remove("start-animC");
                     break;
+
                 case "image3":
-                document.getElementById("image1").classList.add("disappear");
-                document.getElementById("image2").classList.add("disappear");
-                document.getElementById("image4").classList.add("disappear");
+                    document.getElementById(id).classList.remove("start-animB");
                     break;
+
                 case "image4":
-                document.getElementById("image1").classList.add("disappear");
-                document.getElementById("image2").classList.add("disappear");
-                document.getElementById("image3").classList.add("disappear");
+                    document.getElementById(id).classList.remove("start-anim");
                     break;
                 default:
                     break;
             }
-        }else{
+        } else if (action === "add") {
             switch (id) {
                 case "image1":
-                document.getElementById("image2").classList.remove("disappear");
-                document.getElementById("image3").classList.remove("disappear");
-                document.getElementById("image4").classList.remove("disappear");
-    
+                    document.getElementById(id).classList.add("start-animD");
                     break;
                 case "image2":
-                document.getElementById("image1").classList.remove("disappear");
-                document.getElementById("image3").classList.remove("disappear");
-                document.getElementById("image4").classList.remove("disappear");
+                    document.getElementById(id).classList.add("start-animC");
                     break;
+
                 case "image3":
-                document.getElementById("image1").classList.remove("disappear");
-                document.getElementById("image2").classList.remove("disappear");
-                document.getElementById("image4").classList.remove("disappear");
+                    document.getElementById(id).classList.add("start-animB");
                     break;
+
                 case "image4":
-                document.getElementById("image1").classList.remove("disappear");
-                document.getElementById("image2").classList.remove("disappear");
-                document.getElementById("image3").classList.remove("disappear");
+                    document.getElementById(id).classList.add("start-anim");
                     break;
                 default:
                     break;
             }
+
         }
-        
+    }
 
-    }
-    resetCardOpacity = () => {
-        document.getElementById("image1").classList.remove("disappear");
-        document.getElementById("image2").classList.remove("disappear");
-        document.getElementById("image3").classList.remove("disappear");
-        document.getElementById("image3").classList.remove("disappear");
-    }
-    render = () => {
-        return (
-            <div>
-                <div className="sub-text">
-                    <p id="text1" className="subtitle">Home</p>
-                    <p id="text2" className="subtitle">About</p>
-                    <p id="text3" className="subtitle">Labs</p>
-                    <p id="text4" className="subtitle">Contact</p>
-                </div>
-                <div className="scene">
-                    <div className="parent-container">
-                        <div id="card-container" className={`container-3d ${this.state.releaseContain ? "" : "container-3d-width"} overflow-hide-all ${this.state.addclass ? "container-3d-origin" : ""}`}>
-                            <div className="card-3d" >
-                                <img data-ident="1" id="image1" className="image-size image-opacity" onClick={this.handleStateChange} src={PinkImage} alt="atl" />
-                                <img data-ident="2" id="image2" className="image-size image-opacity" onClick={this.handleStateChange} src={PurpleImage} alt="atl" />
-                                <img data-ident="3" id="image3" className="image-size image-opacity" onClick={this.handleStateChange} src={BlueImage} alt="atl" />
-                                <img data-ident="4" id="image4" className="image-size image-opacity image-position-4"   onClick={this.handleStateChange} src={GreenImage} 
-                                alt="atl" />
+        handleSelectionOpacity = (id) => {
+
+            if (this.state.releaseContain) {
+                switch (id) {
+                    case "image1":
+                        document.getElementById("image2").classList.add("disappear");
+                        document.getElementById("image3").classList.add("disappear");
+                        document.getElementById("image4").classList.add("disappear");
+
+                        break;
+                    case "image2":
+                        document.getElementById("image1").classList.add("disappear");
+                        document.getElementById("image3").classList.add("disappear");
+                        document.getElementById("image4").classList.add("disappear");
+                        break;
+                    case "image3":
+                        document.getElementById("image1").classList.add("disappear");
+                        document.getElementById("image2").classList.add("disappear");
+                        document.getElementById("image4").classList.add("disappear");
+                        break;
+                    case "image4":
+                        document.getElementById("image1").classList.add("disappear");
+                        document.getElementById("image2").classList.add("disappear");
+                        document.getElementById("image3").classList.add("disappear");
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                switch (id) {
+                    case "image1":
+                        document.getElementById("image2").classList.remove("disappear");
+                        document.getElementById("image3").classList.remove("disappear");
+                        document.getElementById("image4").classList.remove("disappear");
+
+                        break;
+                    case "image2":
+                        document.getElementById("image1").classList.remove("disappear");
+                        document.getElementById("image3").classList.remove("disappear");
+                        document.getElementById("image4").classList.remove("disappear");
+                        break;
+                    case "image3":
+                        document.getElementById("image1").classList.remove("disappear");
+                        document.getElementById("image2").classList.remove("disappear");
+                        document.getElementById("image4").classList.remove("disappear");
+                        break;
+                    case "image4":
+                        document.getElementById("image1").classList.remove("disappear");
+                        document.getElementById("image2").classList.remove("disappear");
+                        document.getElementById("image3").classList.remove("disappear");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+        }
+        resetCardOpacity = () => {
+            document.getElementById("image1").classList.remove("disappear");
+            document.getElementById("image2").classList.remove("disappear");
+            document.getElementById("image3").classList.remove("disappear");
+            document.getElementById("image3").classList.remove("disappear");
+        }
+        render = () => {
+            return (
+                <div>
+                    <div className="sub-text">
+                        <p id="text1" className="subtitle">Home</p>
+                        <p id="text2" className="subtitle">About</p>
+                        <p id="text3" className="subtitle">Labs</p>
+                        <p id="text4" className="subtitle">Contact</p>
+                    </div>
+                    <div className="scene">
+                        <div className="parent-container">
+                            <div id="card-container" className={`container-3d ${this.state.releaseContain ? "" : "container-3d-width"} overflow-hide-all ${this.state.addclass ? "container-3d-origin" : ""}`}>
+                                <div className="card-3d" >
+                                    <img data-ident="1" id="image1" className="image-size image-opacity" onClick={this.handleStateChange} src={PinkImage} alt="atl" />
+                                    <img data-ident="2" id="image2" className="image-size image-opacity" onClick={this.handleStateChange} src={PurpleImage} alt="atl" />
+                                    <img data-ident="3" id="image3" className="image-size image-opacity" onClick={this.handleStateChange} src={BlueImage} alt="atl" />
+                                    <img data-ident="4" id="image4" className="image-size image-opacity image-position-4" onClick={this.handleStateChange} src={GreenImage}
+                                        alt="atl" />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        )
+            );
+        }
+
     }
 
-}
-
-export default Scene3D;
+    export default Scene3D;
