a42a742e98e6c70ae0354c8c0aa80b443d70d533
George <georgerincon2@gmail.com>
2019-03-04 19:23:27 -0500

    slide out text test successful

--- client/src/components/Scene3D/index.js
+++ client/src/components/Scene3D/index.js
@@ -87,107 +87,117 @@ class Scene3D extends Component {
         }
     }
 
-        handleSelectionOpacity = (id) => {
+    handleSelectionOpacity = (id) => {
 
-            if (this.state.releaseContain) {
-                switch (id) {
-                    case "image1":
-                        document.getElementById("image2").classList.add("disappear");
-                        document.getElementById("image3").classList.add("disappear");
-                        document.getElementById("image4").classList.add("disappear");
+        if (this.state.releaseContain) {
+            switch (id) {
+                case "image1":
+                    document.getElementById("image2").classList.add("disappear");
+                    document.getElementById("image3").classList.add("disappear");
+                    document.getElementById("image4").classList.add("disappear");
 
-                        break;
-                    case "image2":
-                        document.getElementById("image1").classList.add("disappear");
-                        document.getElementById("image3").classList.add("disappear");
-                        document.getElementById("image4").classList.add("disappear");
-                        break;
-                    case "image3":
-                        document.getElementById("image1").classList.add("disappear");
-                        document.getElementById("image2").classList.add("disappear");
-                        document.getElementById("image4").classList.add("disappear");
-                        break;
-                    case "image4":
-                        document.getElementById("image1").classList.add("disappear");
-                        document.getElementById("image2").classList.add("disappear");
-                        document.getElementById("image3").classList.add("disappear");
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                switch (id) {
-                    case "image1":
-                        document.getElementById("image2").classList.remove("disappear");
-                        document.getElementById("image3").classList.remove("disappear");
-                        document.getElementById("image4").classList.remove("disappear");
-
-                        break;
-                    case "image2":
-                        document.getElementById("image1").classList.remove("disappear");
-                        document.getElementById("image3").classList.remove("disappear");
-                        document.getElementById("image4").classList.remove("disappear");
-                        break;
-                    case "image3":
-                        document.getElementById("image1").classList.remove("disappear");
-                        document.getElementById("image2").classList.remove("disappear");
-                        document.getElementById("image4").classList.remove("disappear");
-                        break;
-                    case "image4":
-                        document.getElementById("image1").classList.remove("disappear");
-                        document.getElementById("image2").classList.remove("disappear");
-                        document.getElementById("image3").classList.remove("disappear");
-                        break;
-                    default:
-                        break;
-                }
+                    break;
+                case "image2":
+                    document.getElementById("image1").classList.add("disappear");
+                    document.getElementById("image3").classList.add("disappear");
+                    document.getElementById("image4").classList.add("disappear");
+                    break;
+                case "image3":
+                    document.getElementById("image1").classList.add("disappear");
+                    document.getElementById("image2").classList.add("disappear");
+                    document.getElementById("image4").classList.add("disappear");
+                    break;
+                case "image4":
+                    document.getElementById("image1").classList.add("disappear");
+                    document.getElementById("image2").classList.add("disappear");
+                    document.getElementById("image3").classList.add("disappear");
+                    break;
+                default:
+                    break;
             }
+        } else {
+            switch (id) {
+                case "image1":
+                    document.getElementById("image2").classList.remove("disappear");
+                    document.getElementById("image3").classList.remove("disappear");
+                    document.getElementById("image4").classList.remove("disappear");
+
+                    break;
+                case "image2":
+                    document.getElementById("image1").classList.remove("disappear");
+                    document.getElementById("image3").classList.remove("disappear");
+                    document.getElementById("image4").classList.remove("disappear");
+                    break;
+                case "image3":
+                    document.getElementById("image1").classList.remove("disappear");
+                    document.getElementById("image2").classList.remove("disappear");
+                    document.getElementById("image4").classList.remove("disappear");
+                    break;
+                case "image4":
+                    document.getElementById("image1").classList.remove("disappear");
+                    document.getElementById("image2").classList.remove("disappear");
+                    document.getElementById("image3").classList.remove("disappear");
+                    break;
+                default:
+                    break;
+            }
+        }
 
 
+    }
+    handleTextAppear = (e) => {
+        console.log('ran');
+        if (e.target.id === "image4") {
+            document.getElementById("text1").classList.add("subtitle-appear");
+        } else {
+            return;
         }
-        handleTextAppear = (e) => {
-            console.log('ran');
-            if(e.target.id === "image4"){
-                document.getElementById("text1").classList.add("subtitle-appear");
-            }else{
-                return;
-            }       
 
+    }
+    handleTestReset = (e) => {
+        console.log('out');
+        if(e.target.id === "image4"){
+            document.getElementById("text1").classList.remove("subtitle-appear");
+            document.getElementById("text1").classList.add("subtitle-reset");
+            setTimeout(() => {
+                document.getElementById("text1").classList.remove("subtitle-reset");
+            },1001)
         }
-        resetCardOpacity = () => {
-            document.getElementById("image1").classList.remove("disappear");
-            document.getElementById("image2").classList.remove("disappear");
-            document.getElementById("image3").classList.remove("disappear");
-            document.getElementById("image3").classList.remove("disappear");
-        }
-        render = () => {
-            return (
-                <div>
-                    <div className="sub-text">
+    }
+    resetCardOpacity = () => {
+        document.getElementById("image1").classList.remove("disappear");
+        document.getElementById("image2").classList.remove("disappear");
+        document.getElementById("image3").classList.remove("disappear");
+        document.getElementById("image3").classList.remove("disappear");
+    }
+    render = () => {
+        return (
+            <div>
+                <div className="sub-text">
                     <p id="text1" className="subtitle">Home</p>
-                        <p id="text2" className="subtitle">About</p>
-                        <p id="text3" className="subtitle">Labs</p>
-                        <p id="text4" className="subtitle">Contact</p>
-                    </div>
-                    <div className="scene">
-                        <div className="parent-container">
-                            <div id="card-container" className={`container-3d ${this.state.releaseContain ? "" : "container-3d-width"} overflow-hide-all ${this.state.addclass ? "container-3d-origin" : ""}`}>
-                                <div className="card-3d" >
-                                    <img data-ident="1" id="image1" className="image-size image-opacity" onClick={this.handleStateChange} src={PinkImage} alt="atl" />
-                                    <img data-ident="2" id="image2" className="image-size image-opacity" onClick={this.handleStateChange} src={PurpleImage} alt="atl" />
-                                    <img data-ident="3" id="image3" className="image-size image-opacity" onClick={this.handleStateChange} src={BlueImage} alt="atl" />
-                                    <img data-ident="4" id="image4" className="image-size image-opacity image-position-4" onClick={this.handleStateChange} onMouseOver={this.handleTextAppear} src={GreenImage}
-                                        alt="atl" />
-                                                                            
-                                </div>
+                    <p id="text2" className="subtitle">About</p>
+                    <p id="text3" className="subtitle">Labs</p>
+                    <p id="text4" className="subtitle">Contact</p>
+                </div>
+                <div className="scene">
+                    <div className="parent-container">
+                        <div id="card-container" className={`container-3d ${this.state.releaseContain ? "" : "container-3d-width"} overflow-hide-all ${this.state.addclass ? "container-3d-origin" : ""}`}>
+                            <div className="card-3d" >
+                                <img data-ident="1" id="image1" className="image-size image-opacity" onClick={this.handleStateChange} src={PinkImage} alt="atl" />
+                                <img data-ident="2" id="image2" className="image-size image-opacity" onClick={this.handleStateChange} src={PurpleImage} alt="atl" />
+                                <img data-ident="3" id="image3" className="image-size image-opacity" onClick={this.handleStateChange} src={BlueImage} alt="atl" />
+                                <img data-ident="4" id="image4" className="image-size image-opacity image-position-4" onClick={this.handleStateChange} onMouseOver={this.handleTextAppear} onMouseOut={this.handleTestReset} src={GreenImage}
+                                    alt="atl" />
+
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-            );
-        }
-
+        );
     }
 
-    export default Scene3D;
+}
+
+export default Scene3D;
