e1e735e7ef24a99955c68fdf123ae206231bc18e
George <georgerincon2@gmail.com>
2019-03-08 00:26:36 -0500

    setting up new collapsable menu

---
+++ client/src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse:false
+        }
+    }
+
+    render =() => {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Menu;
