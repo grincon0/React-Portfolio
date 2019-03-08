a84c03a4fdce6016a222228a6db5114f374f79aa
George <georgerincon2@gmail.com>
2019-03-08 14:03:37 -0500

    setting up arrow component

---
+++ client/src/components/Arrow/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import "./style.css";
+
+class Arrow extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            section: '',
+            animate : false,
+            hover: false,
+        }
+
+    }
+    render = () => {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Arrow;
