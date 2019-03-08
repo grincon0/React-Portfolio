55a1f4f9b28aa9cab9ffcb25b55a799d7c0d60c7
George <georgerincon2@gmail.com>
2019-03-07 23:06:28 -0500

    burger icon working as expected

--- client/src/components/NavIcon/index.js
+++ client/src/components/NavIcon/index.js
@@ -7,55 +7,91 @@ class NavIcon extends Component {
         this.state = {
             canReset: false,
             transform: false,
-            hovering:false
-
+            hovering: false,
+            clicked: false,
+            morphX: false
         }
     }
-    componentDidMount = () => {
-            
-        
+    componentDidUpdate = () => {
+
     }
     componentWillUnmount = () => {
-        if(this.state.canReset){
+        if (this.state.canReset) {
             clearTimeout(this.resetTimer);
+            clearTimeout(this.exTimer);
         }
-        
+    }
+    cancelAnim = () => {
+        let resetState = { ...this.state };
+        resetState.transform = false;
+        resetState.clicked = false;
+        resetState.morphX = false;
+
+        clearTimeout(this.resetTimer);
+        clearTimeout(this.exTimer);
+        console.log('anim canceled');
+        this.setState(resetState);
     }
     handleStateChange = () => {
-        this.state.transform ? this.setState({ canReset: false, transform: false }) : this.setState({ canReset: true, transform: true });
-        this.handleTransformReset();
+        console.log(this.state.clicked);
+        if (this.state.clicked) {
+            this.cancelAnim();
+        }
+        this.state.transform ? this.setState({ canReset: false, transform: false, clicked: false }) : this.setState({ canReset: true, transform: true, clicked: true });
+        this.exifyBurger();
     }
     handleTransformReset = () => {
-        
-        //resets state after transition completes
-        this.resetTimer = setTimeout(() => {
+        if (this.state.clicked) {
+            clearTimeout(this.exTimer);
             this.setState({
                 canReset: false,
-                transform: false
-            })
-        }, 901);
+                transform: false,
+                morphX: false,
+                clicked: false
+            });
+        }
+
     }
     handleParentHover = () => {
-        if(!this.state.hovering){
-            let newState = {...this.state};
+        if (!this.state.hovering) {
+            let newState = { ...this.state };
             newState.hovering = true;
             this.setState(newState);
         }
     }
+    handleSetTimeout = (delay, ...args) => {
+        this.resetTimer = setTimeout(() => {
+            this.setState(args);
+        }, (0 + delay));
+    }
     removeParentHover = () => {
-        if(this.state.hovering){
-            let newState = {...this.state};
+        if (this.state.hovering) {
+            let newState = { ...this.state };
             newState.hovering = false;
             this.setState(newState);
         }
     }
+    exifyBurger = () => {
+        if (!this.state.morphX ) {
+            this.exTimer = setTimeout(() => {
+                if(this.state.transform){
+                    let newState = { ...this.state };
+                    newState.morphX = true;
+                    this.setState(newState);
+                }
+            }, 901);
+
+        }
+    }
+
     render = () => {
         return (
-            <div id="burger-icon" 
-            className={`${this.state.hovering ? "hover-burger": ""} ${this.state.transform ? "rotate-in" : ""}`}
-            onClick={this.handleStateChange} 
-            onMouseOver={this.handleParentHover} 
-            onMouseOut={this.removeParentHover}>
+            <div id="burger-icon"
+                className={
+                    `${this.state.hovering ? "hover-burger" : ""} ${this.state.transform ? "rotate-in" : ""} ${this.state.morphX ? "x-out" : ""} `}
+                onClick={this.handleStateChange}
+                onMouseOver={this.handleParentHover}
+                onMouseOut={this.removeParentHover}>
                 <i id="i-first" className={this.state.transform ? "" : ""}></i>
                 <i id="i-second" className={this.state.transform ? " " : ""}></i>
                 <i id="i-third" className={this.state.transform ? "" : ""}></i>
