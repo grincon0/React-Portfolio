c19b93a116fcc7e1c447683a28767773580c1272
George <georgerincon2@gmail.com>
2019-03-11 22:01:29 -0400

    setting up projectcontent section component

---
+++ client/src/pages/sections/ProjectContent.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import AnimatedText from "../../components/AnimatedText/index";
+import Arrow from "../../components/Arrow/index";
+import OverlayBlock from "../../components/OverlayBlock/index";
+import FlexBox from "../../components/Flexbox/index";
+import FlexContainer from "../../components/FlexContainer/index";
+import "./section-styles/projectContent.css";
+
+class ProjectContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            placeholder: ''
+        }
+    }
+
+    render = () => {
+        return (
+            <section>
+
+            </section>
+        );
+    }
+
+}
+
+export default ProjectContent;
+
+
