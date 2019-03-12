cbece18d5ae58b963563b4bac80b5b77140926df
George <georgerincon2@gmail.com>
2019-03-12 01:17:13 -0400

    Overlay block now transforms to top on hover

--- client/src/components/OverlayBlock/index.js
+++ client/src/components/OverlayBlock/index.js
@@ -13,7 +13,7 @@ class OverlayBlock extends Component{
         return(
             <div className="parent-box">
                 <div className={`overlay-content o-full-size ${this.props.classes ? this.props.classes : ""}`}></div>
-                <div className="overlay right"></div>
+                <div className="overlay block-top"></div>
             </div>
 
         );
