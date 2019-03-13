6b74ef4779247898d60e077f50ef76640b5e2973
George <georgerincon2@gmail.com>
2019-03-13 01:03:51 -0400

    overlay blocks first draft is finished

--- client/src/pages/sections/ProjectContent.js
+++ client/src/pages/sections/ProjectContent.js
@@ -14,15 +14,8 @@ class ProjectContent extends Component {
             loaded: false
         }
     }
-    componentDidMount = () => {
 
-
-    }
     render = () => {
-        /*   if(!this.state.loaded){
-              this.setState({loaded: true});
-          }
-           */
 
         return (
             <section id="PROJECTS">
@@ -88,9 +81,9 @@ class ProjectContent extends Component {
                             showClass="overlay-end"
                             color="midnight"
                             delay={400}
-                            appType="Command Line Application"
+                            appType="SQL-backed Command Line Application"
                             headline="Bamazon."
-                            text="Purchase and manage your business' inventory"
+                            text="Purchase and manage your business' inventory with this Node.js powered app"
                             pColorChange="mist-text"
                             h2ColorChange="greenery-text"
                         />
@@ -99,25 +92,12 @@ class ProjectContent extends Component {
                             showClass="overlay-end"
                             color="snorkel"
                             delay={500}
-                            
-                        />
-                        <OverlayBlock
-                            classes={`random-photo-2 align-fix`}
-                            showClass="overlay-end"
-                            color="buttercup"
-                            delay={600}
-                        />
-                        <OverlayBlock
-                            classes={`random-photo align-fix`}
-                            showClass="overlay-end"
-                            color="eclipse"
-                            delay={700}
-                        />
-                        <OverlayBlock
-                            classes={`random-photo-3 align-fix`}
-                            showClass="overlay-end"
-                            color="meadow"
-                            delay={800}
+                            appType="Command Line Application"
+                            headline="Liri-Bot."
+                            text="Run a number of queries and return data wiht Liri"
+                            pColorChange="buttercup-text"
+                            h1ColorChange="sun-text"
+                            h2ColorChange="bright-text"
                         />
 
                     </FlexBox>
