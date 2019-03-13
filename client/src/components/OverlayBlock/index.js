2dc1adedc26183855f0ce41418053519fbf82f68
George <georgerincon2@gmail.com>
2019-03-12 21:25:14 -0400

    league spartan + finalizing overlay info

--- client/src/components/OverlayBlock/index.js
+++ client/src/components/OverlayBlock/index.js
@@ -19,23 +19,28 @@ class OverlayBlock extends Component {
         clearTimeout(this.timerTransform);
     }
     handleHover = () => {
-        this.setState({hover:true});
+        this.setState({ hover: true });
     }
     handleOut = () => {
-        this.setState({hover:false});
+        this.setState({ hover: false });
     }
     render = () => {
         return (
-            <div id="overlay-parent" className={`parent-box ${this.state.hover? "show-block-text" : ""}`} onMouseOver={this.handleHover} onMouseOut={this.handleOut}>
-                <div className={`overlay-content o-full-size overlay-start ${this.props.classes ? this.props.classes : ""} ${this.state.show ? this.props.showClass : ""}`}></div>
+            <div id="overlay-parent" className={`parent-box ${this.state.hover ? "show-block-text" : ""}`} onMouseOver={this.handleHover} onMouseOut={this.handleOut}>
+
+
+                <div className={`overlay-content o-full-size overlay-start ${this.props.classes ? this.props.classes : ""} ${this.state.show ? this.props.showClass : ""}`}>
+
+                </div>
                 <div id="content" className={`overlay block-top ${this.props.color ? this.props.color : ""} `}>
 
                     <div id="text-box" className="text-box-cls text-box-start">
-                        <p>{this.props.appType}</p>
-                        <h1>{this.props.headline}</h1>
-                        <p>{this.props.text}</p>
-
                     </div>
+                </div>
+                <div id="overlay-info" className={`pos-start headline ${this.state.hover ? "move-headline" : ""}`}>
+                    <p>{this.props.appType}</p>
+                    <h1>{this.props.headline}</h1>
+                    <h2 className={`hidden-text overlay-subtitle ${this.state.hover ? "show-text-o" : ""}`}>{this.props.text}</h2>
 
                 </div>
             </div>
