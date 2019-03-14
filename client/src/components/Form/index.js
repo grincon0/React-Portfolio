a5f1f8e03497927bfd1bf9e19e752d6c078e9559
George <georgerincon2@gmail.com>
2019-03-14 01:17:49 -0400

    styling contact form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -13,7 +13,7 @@ class Form extends Component {
         }
     }
     handleInputChange = event => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
 
         this.setState({
             [name]: value
@@ -22,49 +22,50 @@ class Form extends Component {
     handleFormSubmit = event => {
         event.preventDefault();
         console.log(this.state.msg);
-        this.setState({name: "", email: "", msg: ""})
+        this.setState({ name: "", email: "", msg: "" })
     }
 
     render = () => {
         return (
-            <form>
+            <form id="form">
                 <div>
-
-                    <input 
-                    type="text" 
-                    placeholder="Enter your name."
-                    id="name" 
-                    className="input-bg" 
-                    name="name" 
-                    value={this.state.name}
-                    onChange={this.handleInputChange}
+                    <input
+                        type="text"
+                        placeholder=" Enter your name."
+                        id="name"
+                        className="input-bg form-start underline"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleInputChange}
                     />
 
                 </div>
                 <div>
 
-                    <input 
-                    type="email" 
-                    placeholder="Enter your email address."
-                    id="email" 
-                    className="input-bg" 
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                     />
+                    <input
+                        type="email"
+                        placeholder=" Enter your email address."
+                        id="email"
+                        className="input-bg form-start underline"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleInputChange}
+                    />
                 </div>
                 <div>
 
-                    <textarea 
-                    id="msg" 
-                    placeholder="Your message."
-                    className="input-bg" 
-                    name="msg"
-                    value={this.state.msg}
-                    onChange={this.handleInputChange}
+                    <textarea
+                        id="msg"
+                        placeholder=" Your message."
+                        className="input-bg form-start underline"
+                        name="msg"
+                        value={this.state.msg}
+                        onChange={this.handleInputChange}
                     ></textarea>
                 </div>
-                <button onClick={this.handleFormSubmit}>Submit</button>
+                <div className="btn-box">
+                    <button onClick={this.handleFormSubmit}>Submit</button>
+                </div>
             </form>
 
         );
