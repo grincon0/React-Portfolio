c22bba79f65ab8d9d115710aa6bf0958db7f5c1f
George <georgerincon2@gmail.com>
2019-03-14 17:54:58 -0400

    restruct for router => contact

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Wrapper from "./components/Wrapper";
 import Test3D from "./pages/Test3D";
 import About from "./pages/sections/About";
+import Contact from "./pages/Contact";
 import "./App.css";
 
 
@@ -11,12 +13,20 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
         <Wrapper>
-         {/* <Test3D  /> */}
-          
-         <About />
+          <Switch>
+            {/*             <Route exact path="/" component={Test3D} /> */}
+            {/*             <Route exact path="/about" component={About} /> */}
+            <Route exact path="/" component={Contact} />
+
+          </Switch>
+          {/* <Test3D  /> */}
+
+          {/* <About /> */}
         </Wrapper>
-      
+      </Router>
+
     );
   }
 }
