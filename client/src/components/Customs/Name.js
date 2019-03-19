ab76e2c7074473915c8510e01e170243baeed518
George <georgerincon2@gmail.com>
2019-03-19 16:17:18 -0400

    new comps + editing Name animations

---
+++ client/src/components/Customs/Name.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/Name.css";
+
+export const Name = (props) => {
+    return (
+        <section id="Name" className={`Animated`}>
+            <h1>
+                <span>G</span>
+                <span>E</span>
+                <span>O</span>
+                <span>R</span>
+                <span>G</span>
+                <span>E</span>
+            </h1>
+        </section>
+
+    );
+}
