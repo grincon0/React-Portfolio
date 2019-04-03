a566b5fab831cef21036f25eef1c9a93b41e91d7
George <georgerincon2@gmail.com>
2019-04-03 16:00:30 -0400

    fixed overlay width with calc :)

--- client/src/components/Lists/ButtonList.js
+++ client/src/components/Lists/ButtonList.js
@@ -8,58 +8,18 @@ export class ButtonList extends Component {
         this.state = {
             test: false,
             count: 9,
-            content: [{ name: "ReactJS" }, { name: "NodeJS" }, { name: "JavaScript" }, { name: "BackEnd" }, { name: "Mongodb" },
+            content: [{ name: "ReactJS" }, { name: "NodeJS" }, { name: "JavaScript" }, { name: "FrontEnd" }, { name: "BackEnd" }, { name: "Mongodb" },
             { name: "Game Design" }, { name: "AR" }, { name: "React Native" }, { name: "Sockets.io" }]
         }
     }
-
-
     dumpListItems = () => {
         return this.state.content.map((item) => <li><Button text={item.name} /></li>)
     }
-
     render = () => {
         let dumpList = this.dumpListItems();
         return (
             <ul className={`button-list`}>
                 {dumpList}
-                {/*                     <li>
-                        <Button
-                            text="ReactJS"
-                        />
-                    </li>
-
-
-                    <li>
-                        <Button
-                            text="Node.Js"
-                        />
-                    </li>
-                    <li>
-                        <Button
-                            text="Javascript"
-                        />
-                    </li>
-                    <li>
-                        <Button
-                            text="Mongodb"
-                        />
-                    </li>
-                    <li>
-                        <Button
-                            text="Game Design"
-                        />
-                    </li>
-                    <li>
-                        <Button
-                            text="Augmented Reality"
-                        />
-                    </li>
-                    <li>
-
-                        <Button
-                            text="React-Native" />
-                    </li> */}
             </ul>
         );
     }
