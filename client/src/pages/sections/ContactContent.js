6688bc7d1abeb45060aa6159128278f423eb62b6
George <georgerincon2@gmail.com>
2019-04-04 15:07:18 -0400

    FIXED the contact title overlay bug, REMEMBER: ::before needs to be at the text destination, have its parent element translate upwards to it to give the text an internal overlay effect :)

--- client/src/pages/sections/ContactContent.js
+++ client/src/pages/sections/ContactContent.js
@@ -19,20 +19,27 @@ class ContactContent extends Component {
     render = () => {
         return (
             <section id="CONTACT">
-            <FlexContainer
-                direction="column"
-                justify="center"
-                classes="align-fix">
-                <div className="contact-title-div">
-                    <h1 className="contact-title">Contact</h1>
-                </div>
-                <div>
-                    <p className="contact-subtitle">Want to <span className="highlight-2">work together</span>? Need a <span className="highlight-1">developer</span> for your <span className="highlight-3">team</span>? Let's talk.</p>
-                </div>
-                <Form/>
-         
-            </FlexContainer>
-                
+                <FlexContainer
+                    direction="column"
+                    justify="center"
+                    classes="align-fix">
+                    <div className="testi">
+                        <div className="contact-title-div">
+                            <h1 className="contact-title">Contact</h1>
+                            <div className="contact-title-front">
+                                <h1 data-content="Contact"className="contact-title-overlay">Contact</h1>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <p className="contact-subtitle">Want to <span className="highlight-2">work together</span>? Need a <span className="highlight-1">developer</span> for your <span className="highlight-3">team</span>? Let's talk.</p>
+                    </div>
+                    <Form />
+
+                </FlexContainer>
+
             </section>
         );
     }
