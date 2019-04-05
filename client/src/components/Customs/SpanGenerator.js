3833434d0ea725fa57e2e337b994a9407c600393
George <georgerincon2@gmail.com>
2019-04-05 01:09:16 -0400

    Span Generator fully functional + fully animated for Contact page

--- client/src/components/Customs/SpanGenerator.js
+++ client/src/components/Customs/SpanGenerator.js
@@ -1,49 +1,109 @@
 import React, { Component } from "react";
+import "./styles/SpanGenerator.css";
 
 export class SpanGenerator extends Component {
     constructor(props) {
         super(props);
         this.state = {
             strings: [],
-            styledSpans: []
+            styledSpans: [],
+            animate: false,
+
+
         }
     }
 
     componentDidMount = () => {
+
         if (this.props.text) {
             const split = this.splitTextProp(this.props.text);
             if (this.props.blocks) {
-                let newState = {...this.state}
-                console.log(split);
-                console.log(this.props.blocks);
+                let newState = { ...this.state }
+
                 newState.styledSpans = this.getSpanBlocks(split, this.props.blocks);
-                console.log(newState);
+
                 this.setState(newState);
-                
+
             } else {
                 this.setState({ strings: split });
             }
 
         }
 
-    }
-    componentDidUpdate = () => {
-        console.log('comp updated');
+
+        /*  this.animateSpans(); */
 
     }
-    getSpanBlocks = (slicedStrings, { from, to, classes }) => {
+
+
+    /* animateSpans = () => {
+        if (this.state.styledSpans) {
+            let e;
+            let ancestor = document.getElementById("Span-Container"),
+                descendents = ancestor.getElementsByTagName('*');
+
+            for (let i = 0; i < descendents.length; i++) {
+                e = descendents[i];
+                e.className += " justdo";
+
+            }
+        }
+    } */
+
+
+    getSpanBlocks = (slicedStrings, arr) => {
+
         if (slicedStrings !== undefined) {
-            return slicedStrings.map((span, i) => {
-                if ((from - 1) <= i && i <= (to - 1)) {
-                    return <span key={i} className={classes}>{span}</span>;
+            const spansArray = [];
+            const chars = slicedStrings;
+            let counter = 0;
+
+            console.log(chars);
+
+
+            for (let i = 0; i < chars.length; i++) {
+
+                let elementDisplay = this.checkIfEmpty(chars[i]);
+                console.log(elementDisplay)
+
+
+                if (counter < arr.length && arr[counter].from <= i && i <= arr[counter].to) {
+
+
+
+                    spansArray.push(<span 
+                        id={`s-${i}`} 
+                        style={{display:`${elementDisplay}`, animationName:"revealSpan", animationDuration:"0.6s" ,animationTimingFunction:"ease", animationFillMode:"forwards"}} 
+                        key={i} 
+                        className={arr[counter].classes}>{chars[i]}</span>);
+
+                    if (i === arr[counter].to && arr[counter].completed === false) {
+
+
+                        arr[counter].completed = true;
+                        counter++;
+
+
+
+                    }
                 } else {
-                    return <span key={i}>{span}</span>;
+                    spansArray.push(<span style={{ display:`${elementDisplay}`, animationName:"revealSpan", animationDuration:"0.6s" ,animationTimingFunction:"ease", animationFillMode:"forwards" }} id={`s-${i}`} key={i}>{chars[i]}</span>);
                 }
             }
-            );
+
+            return spansArray;
         } else {
             return;
         }
+
+    }
+    checkIfEmpty = (str) => {
+        if (!str.replace(/\s/g, '').length) {
+            return "inline"
+        } else {
+            return "inline-block"
+        }
+
     }
 
     splitTextProp = (text) => {
@@ -56,12 +116,14 @@ export class SpanGenerator extends Component {
 
     }
     render = () => {
-        let spans; 
 
-         spans = !this.state.styledSpans ? this.mapSpans() : this.state.styledSpans;
-        
+        /*    setTimeout(() => {
+               this.animateSpans();
+           },3000); */
+        let spans = !this.state.styledSpans ? this.mapSpans() : this.state.styledSpans;
+
         return (
-            <div>
+            <div id="Span-Container" className={`span-default`}>
                 {spans}
             </div>
 
